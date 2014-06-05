84d450d8a78bc20e2955593d1c282097d07d53e2
James White <dev.jameswhite@gmail.com>
2014-06-05 03:51:05 +0100

    Trailing spaces

--- Assets/Scripts/Click.js
+++ Assets/Scripts/Click.js
@@ -15,15 +15,15 @@ function FixedUpdate () {
 	if (clicked) {
 		// Add a scaled up upwards force of KickPower to simulate a kick
 		rigidbody.AddForce(Vector3.up * (KickPower * 100));
-		
+
 		// Increment the users score
 		menu.GetComponent(Menu).CurrentScore += 1;
-		
+
 		// Update the high score if the current score is higher than the previous high score
 		if (menu.GetComponent(Menu).CurrentScore > menu.GetComponent(Menu).HighScore) {
 			menu.GetComponent(Menu).HighScore = menu.GetComponent(Menu).CurrentScore;
 		}
-		
+
 		// Unclick the ball so we don't process it again until it has been clicked again
 		clicked = false;
 	}
